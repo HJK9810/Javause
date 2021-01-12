3e6c86317ddb1819cbf6ef7251cf0e044a40a41f
HJK9810 <ruby45342@gamail.com>
2021-01-12 10:24:45 +0900

    GUI setting

--- src/practice/OrderMenuGUI.java
+++ src/practice/OrderMenuGUI.java
@@ -1,10 +1,96 @@
 package practice;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class OrderMenuGUI extends JPanel {
-    private JLabel message;
-    public OrderMenuGUI() {
 
+}
+class WelcomeP extends JPanel {
+
+    private JLabel message;
+    public WelcomeP() {
+        message = new JLabel("Welcome to Pizzastore");
+    }
+//    add(message);
+}
+class TypeP extends JPanel {
+
+    private JRadioButton combo, potato, bulgogi;
+    public TypeP() {
+        setLayout(new GridLayout(3, 1));
+        combo = new JRadioButton("콤보", true);
+        potato = new JRadioButton("포테이토");
+        bulgogi = new JRadioButton("불고기");
+
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(combo);
+        bg.add(potato);
+        bg.add(bulgogi);
+
+        setBorder(BorderFactory.createTitledBorder("종류"));
+        add(combo);
+        add(potato);
+        add(bulgogi);
+    }
+}
+class ToppingP extends JPanel {
+
+    private JRadioButton pimang, cheeze, peplony, bacon;
+    public ToppingP() {
+        setLayout(new GridLayout(4, 1));
+        pimang = new JRadioButton("피망", true);
+        cheeze = new JRadioButton("치즈");
+        peplony = new JRadioButton("페페로니");
+        bacon = new JRadioButton("베어컨");
+
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(pimang);
+        bg.add(cheeze);
+        bg.add(peplony);
+        bg.add(bacon);
+
+        setBorder(BorderFactory.createTitledBorder("추가 토핑"));
+        add(pimang);
+        add(cheeze);
+        add(peplony);
+        add(bacon);
+    }
+}
+class SizeP extends JPanel {
+
+    private JRadioButton small, meddium, large;
+    public SizeP() {
+        setLayout(new GridLayout(3, 1));
+        small = new JRadioButton("Small", true);
+        meddium = new JRadioButton("Medium");
+        large = new JRadioButton("Large");
+
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(small);
+        bg.add(meddium);
+        bg.add(large);
+
+        setBorder(BorderFactory.createTitledBorder("종류"));
+        add(small);
+        add(meddium);
+        add(large);
+    }
+}
+class OrderP extends JPanel {
+
+    private JButton order, cancle;
+    private JTextField cost;
+    public OrderP() {
+        order = new JButton("주문");
+        cancle = new JButton("취소");
+
+        cost = new JTextField();
+        cost.setEditable(false);
+        cost.setColumns(6);
+
+        add(order);
+        add(cancle);
+        add(cost);
     }
 }
