13b70f5f7171cf12c65e38102e702b783c190558
HJK9810 <ruby45342@gamail.com>
2021-01-28 14:28:35 +0900

    make how calculate num

--- src/practice/Calculate.java
+++ src/practice/Calculate.java
@@ -1,29 +1,31 @@
 package practice;
 
 public class Calculate {
-    private int num1, num2;
-    private String org;
+    private double num1, num2;
+    private String oper;
+    public Calculate() {}
 
-    public Integer Calculate(int n1, int n2, String s) {
-        num1 = n1;
-        num2 = n2;
-        org = s;
-
-        switch (s) {
+    public void setCal(String n1, String n2, String s) {
+        num1 = Double.parseDouble(n1);
+        num2 = Double.parseDouble(n2);
+        oper = s;
+    }
+    double compute() {
+        switch (oper) {
             case "+":
                 return num1 + num2;
             case "-":
                 return num1 - num2;
-            case  "*":
+            case  "X":
                 return num1 * num2;
             case "/":
                 if (num2 == 0) {
-                    return null;
+                    return Double.NaN;
                 } else {
                     return num1 / num2;
                 }
             default:
-                return null;
+                return Double.NaN;
         }
     }
 }
--- src/practice/ClickCalculGUI.java
+++ src/practice/ClickCalculGUI.java
@@ -11,7 +11,6 @@ public class ClickCalculGUI extends JFrame {
 
     public ClickCalculGUI() {
         setTitle("Calculater");
-        setSize(500, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         Container c = getContentPane();
@@ -19,16 +18,16 @@ public class ClickCalculGUI extends JFrame {
 
         c.add(st, BorderLayout.NORTH);
         c.add(cbg, BorderLayout.CENTER);
-        cbg.setLayout(null);
 
-//        pack();
+        pack();
         setVisible(true);
     }
     class ShowTxt extends JPanel {
         private JTextField display;
         public ShowTxt() {
-            display = new JTextField(35);
+            display = new JTextField(40);
             display.setText("0.0");
+            display.setEnabled(false);
             add(display);
         }
     }
@@ -40,25 +39,44 @@ public class ClickCalculGUI extends JFrame {
                 "4", "5", "6", "-", "1/x",
                 "7", "8", "9", "X", "!",
                 "+/-", "0", ".", "/", "="};
+        ShowTxt txt = new ShowTxt();
+        Calculate cal = new Calculate();
 
         public CalButtonsGruop() {
-            setLayout(new GridLayout(5, 5, 3, 3));
+            setLayout(new GridLayout(0, 5, 3, 3));
 
             for (int i = 0 ; i < buttons.length ; i++) {
                 buttons[i] = new JButton(label[i]);
-                add(buttons[i]);
-                buttons[i].addActionListener(this);
 
-                if (i % 5 >= 3)
+                if (i % 5 >= 3) {
                     buttons[i].setForeground(Color.red);
-                else
+                } else {
                     buttons[i].setForeground(Color.black);
+                }
 
                 buttons[i].setBackground(Color.orange);
+                add(buttons[i]);
+                buttons[i].addActionListener(this);
             }
         }
         public void actionPerformed (ActionEvent e) {
-            Object command = e.getActionCommand();
+            String command = e.getActionCommand();
+            String[] str;
+            double result;
+            String oper = null;
+
+            if (command.charAt(0) == 'C') {
+                txt.display.setText("0.0");
+            } else if (command.charAt(0) >= '0' && command.charAt(0) <= '9' || command.charAt(0) == '.'){
+                txt.display.setText(command);
+            } else if (command.charAt(0) == '='){
+                str = txt.display.getText().split(oper);
+                cal.setCal(str[0], str[1], oper);
+                result = cal.compute();
+                txt.display.setText(Double.toString(result));
+            } else {
+                oper = command;
+            }
         }
     }
 }
